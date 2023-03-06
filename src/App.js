d3ed211261be3b52e6f757fabb22b17b5937bd93
Ibn-abdillah <akinolasheriff2022@gmail.com>
2023-03-06 18:26:38 +0100

    import the firstContent component to the app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import FirstContent from './components/firstContent/firstContent';
 import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <FirstContent />
     </div>
   );
 }
